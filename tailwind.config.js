80277e3e7df8273ddf25fd412e312ba1a5bb305e
Allen Thomas <allen.thomas.dev@gmail.com>
2024-04-11 11:31:54 +0530

    minimal af rework

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['CustomFont', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
